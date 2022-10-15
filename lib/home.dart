dd0e522bb1767edcc6a858b1648a0bec49f77ed2
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-15 13:49:19 +0500

    Copy and Pasting entire Scaffold of `Sliding Sheet`  from pub dev.

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,40 @@ class MyHome extends StatefulWidget {
 class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        title: Text('Simple Example'),
+      ),
+      body: SlidingSheet(
+        elevation: 8,
+        cornerRadius: 16,
+        snapSpec: const SnapSpec(
+          // Enable snapping. This is true by default.
+          snap: true,
+          // Set custom snapping points.
+          snappings: [0.4, 0.7, 1.0],
+          // Define to what the snappings relate to. In this case,
+          // the total available space that the sheet can expand to.
+          positioning: SnapPositioning.relativeToAvailableSpace,
+        ),
+        // The body widget will be displayed under the SlidingSheet
+        // and a parallax effect can be applied to it.
+        body: Center(
+          child: Text('This widget is below the SlidingSheet'),
+        ),
+        builder: (context, state) {
+          // This is the content of the sheet that will get
+          // scrolled, if the content is bigger than the available
+          // height of the sheet.
+          return Container(
+            height: 500,
+            child: Center(
+              child: Text('This is the content of the sheet'),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
