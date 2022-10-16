5fc6160b37c2112ae39be0001508a921208eb94b
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-16 20:22:10 +0500

    Basic Structuring and importing in about.dart

---
+++ lib/about.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_flutter/about.dart';
+
+class MyAbout extends StatefulWidget {
+  const MyAbout({Key? key}) : super(key: key);
+
+  @override
+  State<MyAbout> createState() => _MyAboutState();
+}
+
+class _MyAboutState extends State<MyAbout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
