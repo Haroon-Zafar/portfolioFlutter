e9342a29278aa1023415b1b376f0b486114d2daa
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-16 21:18:11 +0500

    Container() 's code is copied.

--- lib/about.dart
+++ lib/about.dart
@@ -18,6 +18,61 @@ class _MyAboutState extends State<MyAbout> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
+      body: Container(
+        // why using Stack() instead of Column() ? because we can put widgets on top of each other.
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                right: 80,
+              ),
+              child: ShaderMask(
+                shaderCallback: (rect) {
+                  return LinearGradient(
+                    begin: Alignment.center,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.black, Colors.transparent],
+                  ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                },
+                blendMode: BlendMode.dstIn,
+                child: Image.asset(
+                  'assets/img.png',
+                  // height: 400,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.49),
+              child: Column(
+                children: [
+                  Text(
+                    "Haroon Zafar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Web Developer",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
