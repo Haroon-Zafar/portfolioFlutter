186eb9a4aaaed54bc46dd3ee6cf454cdf5fc38b3
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-16 20:20:49 +0500

    Created about.dart

--- lib/home.dart
+++ lib/home.dart
@@ -101,33 +101,46 @@ class _MyHomeState extends State<MyHome> {
                   right: 80,
                 ),
                 child: ShaderMask(
-                    shaderCallback: (rect) {
-                      return LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.black, Colors.transparent],
-                      ).createShader(
-                          Rect.fromLTRB(0, 0, rect.width, rect.height));
-                    },
-                    blendMode: BlendMode.dstIn,
-                    child: Image.asset(
-                      'assets/img.png',
-                      height: 400,
-                      fit: BoxFit.contain,
-                    )),
+                  shaderCallback: (rect) {
+                    return LinearGradient(
+                      begin: Alignment.center,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.black, Colors.transparent],
+                    ).createShader(
+                        Rect.fromLTRB(0, 0, rect.width, rect.height));
+                  },
+                  blendMode: BlendMode.dstIn,
+                  child: Image.asset(
+                    'assets/img.png',
+                    // height: 400,
+                    fit: BoxFit.contain,
+                  ),
+                ),
               ),
               Container(
+                alignment: Alignment.center,
                 margin: EdgeInsets.only(
                     top: MediaQuery.of(context).size.height * 0.49),
                 child: Column(
                   children: [
                     Text(
-                      "Muhammad Haroon Zafar",
-                      style: TextStyle(color: Colors.white),
+                      "Haroon Zafar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
                     ),
                     Text(
                       "Web Developer",
-                      style: TextStyle(color: Colors.white),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
