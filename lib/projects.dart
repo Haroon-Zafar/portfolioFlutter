b90513386625c3020d4de9b3468cee1e20d4c542
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-17 18:55:14 +0500

    Created projects.dart and import necessary material.dart

---
+++ lib/projects.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyProjects extends StatefulWidget {
+  const MyProjects({ Key? key }) : super(key: key);
+
+  @override
+  State<MyProjects> createState() => _MyProjectsState();
+}
+
+class _MyProjectsState extends State<MyProjects> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
