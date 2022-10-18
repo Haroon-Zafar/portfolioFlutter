eae959c687e69a11043434b9bddb76f48dd75a16
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-18 19:44:18 +0500

    myProjects() function for Code Reusability.

--- lib/projects.dart
+++ lib/projects.dart
@@ -9,6 +9,86 @@ class MyProjects extends StatefulWidget {
 }
 
 class _MyProjectsState extends State<MyProjects> {
+  // Making myProjects function for Code-Reusability.
+  myProjects() {
+    return Container(
+      height: 220,
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: Card(
+        color: Color(0xff262628),
+        child: Container(
+          margin: EdgeInsets.only(
+            left: 20,
+            right: 10,
+            top: 40,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Flutter",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Click 2 Code",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Text(
+                "An Online Compiler",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.white70,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    "10",
+                    style: TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                  //Expanded is used to place the github icon at the top most right.
+                  Expanded(child: Container()),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      FontAwesomeIcons.github,
+                      color: Colors.white70,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
